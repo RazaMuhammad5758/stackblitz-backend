e2588451e1e30b067da150d5aaced4b4bb97a841
Raza Muhammad ☑️ <waistband161@gmail.com>
2024-10-05 14:07:07 +0500

    schema making done

--- models/todos/sub_todos.models.js
+++ models/todos/sub_todos.models.js
@@ -1,5 +1,18 @@
 import mongoose from " mongoose"
 
-const sub_todoSchema = new mongoose.Schema({}, {timestamps: true})
+const sub_todoSchema = new mongoose.Schema({
+    content:{
+        type: String,
+        required: true
+    },
+    complete:{
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schemas.Types.OpjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
 
 export const Sub_todo = mongoose.model("Sub_todo", sub_todoSchema) 
--- models/todos/todos.models.js
+++ models/todos/todos.models.js
@@ -13,13 +13,13 @@ const todoSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,  // jb b refernece btana hoga to type ese hi likhengy
         ref: "User"   // reference user.models k User se uthaega
     },
-    subTodos: [
+    subTodos: [ 
 
         {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Sub_todo"
         }
-    ]
+    ] // array of sub_todos
 }, {timestamps: true})
 
 export const Todo = mongoose.models("Todos", todoSchema)
